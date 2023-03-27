bd4c014085ee740726ddbba5c85e153bf138278a
rutul30 <94869513+rutul30@users.noreply.github.com>
2023-03-27 14:22:34 -0400

    latest

--- js/config.js
+++ js/config.js
@@ -1,10 +1,11 @@
 window._config = {
   cognito: {
-    userPoolId: "us-east-1_MX1zjZbgi", // e.g. us-east-2_uXboG5pAb
-    userPoolClientId: "3jq7qohgj4kf8359ggj1pcvl46", // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+    userPoolId: "us-east-1_DWeJwte0Q", // e.g. us-east-2_uXboG5pAb
+    userPoolClientId: "38via75nt8gfdh04577fe43vps", // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
     region: "us-east-1", // e.g. us-east-2
   },
   api: {
-    invokeUrl: "https://grspkumd6j.execute-api.us-east-1.amazonaws.com/prod", // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
+    invokeUrl: "https://main.dezey6vhy1vmy.amplifyapp.com/", // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
   },
 };
+lates
